53aeb3e1e7a95fe43e037958940e7a623be9e8bc
No Need <77963254+G8ur@users.noreply.github.com>
2024-01-11 12:01:54 +0530

    done

---
+++ admin-dash/src/scenes/global/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div>
+            sidebar
+            {/* Sidebar content goes here */}
+        </div>
+    );
+};
+
+export default Sidebar;
---
+++ admin-dash/src/scenes/global/Topbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Topbar = () => {
+    return (
+        <div>
+            Topbar
+            {/* Sidebar content goes here */}
+        </div>
+    );
+};
+
+export default Topbar;
